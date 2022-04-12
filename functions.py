79a14867a4083839c43123ed8fd30aa5f8bf56a3
chrismarquez <christophersmarquez@outlook.com>
2022-04-12 19:12:12 +0100

    feat: implement rosenbrock function, plots

--- functions.py
+++ functions.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
-from typing import Callable
+from typing import Callable, Union
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+Real = Union[np.ndarray, float]
+Function2D = Callable[[Real, Real], Real]
 
-def ackley(x: np.ndarray | float, y: np.ndarray | float) -> np.ndarray | float:
+
+def ackley(x: Real, y: Real) -> Real:
     terms = [
         - 20.0 * np.exp(-0.2 * np.sqrt(0.5 * (x ** 2 + y ** 2))),
         - np.exp(0.5 * (np.cos(2 * np.pi * x) + np.cos(2 * np.pi * y))),
@@ -15,7 +18,13 @@ def ackley(x: np.ndarray | float, y: np.ndarray | float) -> np.ndarray | float:
     return sum(terms)
 
 
-def plot(fn: Callable[[np.ndarray | float, np.ndarray | float], np.ndarray | float]):
+def rosenbrock(x: Real, y: Real):
+    a = 1
+    b = 100
+    return (a - x) ** 2 + b * (y - x ** 2) ** 2
+
+
+def plot(fn: Function2D):
     r_min, r_max = -32.768, 32.768
     xaxis = np.arange(r_min, r_max, 2.0)
     yaxis = np.arange(r_min, r_max, 2.0)
@@ -34,3 +43,4 @@ def plot(fn: Callable[[np.ndarray | float, np.ndarray | float], np.ndarray | flo
 if __name__ == '__main__':
     print(ackley(0.0, 0.0))
     plot(fn=ackley)
+    plot(fn=rosenbrock)
