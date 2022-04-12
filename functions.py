4f186f54a40d909f6c327f5abc1ffc579109a5f3
chrismarquez <christophersmarquez@outlook.com>
2022-04-12 19:35:24 +0100

    feat: implement rastrigin function, plots

--- functions.py
+++ functions.py
@@ -24,23 +24,30 @@ def rosenbrock(x: Real, y: Real):
     return (a - x) ** 2 + b * (y - x ** 2) ** 2
 
 
-def plot(fn: Function2D):
-    r_min, r_max = -32.768, 32.768
-    xaxis = np.arange(r_min, r_max, 2.0)
-    yaxis = np.arange(r_min, r_max, 2.0)
+def rastrigin(x: Real, y: Real) -> Real:
+    term = x ** 2 + y ** 2 - 10 * (np.cos(2 * np.pi * x) + np.cos(2 * np.pi * y))
+    return 20 + term
+
+
+def plot(fn: Function2D, x_max=5.12, n_points=100):
+    r_min, r_max = -x_max, x_max
+    spacing = 2 * x_max / n_points
+    xaxis = np.arange(r_min, r_max, spacing)
+    yaxis = np.arange(r_min, r_max, spacing)
     x, y = np.meshgrid(xaxis, yaxis)
     results: np.ndarray = fn(x, y)
     figure = plt.figure()
     axis = figure.gca(projection='3d')
     axis.plot_surface(x, y, results, cmap='jet', shade="false")
     plt.show()
-    plt.contour(x, y, results)
-    plt.show()
-    plt.scatter(x, y, results)
-    plt.show()
+    # plt.contour(x, y, results)
+    # plt.show()
+    # plt.scatter(x, y, results)
+    # plt.show()
 
 
 if __name__ == '__main__':
-    print(ackley(0.0, 0.0))
-    plot(fn=ackley)
-    plot(fn=rosenbrock)
+    print(rastrigin(0.0, 0.0))
+    plot(fn=ackley, x_max=32.768)
+    plot(fn=rosenbrock, x_max=32.768)
+    plot(fn=rastrigin)
